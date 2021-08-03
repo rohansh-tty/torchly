229e1d076cc8757867564810e2079c6d67a1d3a5
Rohan Shetty <40785407+Gilf641@users.noreply.github.com>
2021-08-03 23:16:16 +0530

    Update resnet_model.py

--- models/resnet_model.py
+++ models/resnet_model.py
@@ -148,7 +148,7 @@ class ResNet(nn.Module):
         self,
         block: Type[Union[BasicBlock, Bottleneck]],
         layers: List[int],
-        num_classes: int = 1000,
+        num_classes: int = 200,
         zero_init_residual: bool = False,
         groups: int = 1,
         width_per_group: int = 64,
